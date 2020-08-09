3a72add4264f47c80882d9b3eaeb0a81cd6cc791
huomeng <455801665@qq.com>
2020-08-09 22:39:52 +0800

    点击标签可到编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,14 @@
 <template>
     <Layout>
-        <ul class="tags">
-            <li v-for="tag in tags" :key="tag.id">
+        <div class="tags">
+            <router-link class="tag"
+                         v-for="tag in tags" :key="tag.id"
+                         :to="`/labels/edit/${tag.id}`">
                 <span>{{tag.name}}</span>
                 <Icon name="right"/>
-            </li>
+            </router-link>
 
-        </ul>
+        </div>
         <div class="createTag-wrapper">
             <button class="createTag" @click="createTag">新建标签</button>
         </div>
@@ -26,11 +28,11 @@
     createTag() {
       const name = window.prompt('请输入标签名');
       if (name) {
-        const message = tagListModel.create(name)
-        if (message==='duplicated'){
-            window.alert('标签名重复')
-        }else if (message==='success'){
-          window.alert('添加成功')
+        const message = tagListModel.create(name);
+        if (message === 'duplicated') {
+          window.alert('标签名重复');
+        } else if (message === 'success') {
+          window.alert('添加成功');
         }
       }
     }
@@ -43,7 +45,7 @@
         font-size: 16px;
         padding-left: 16px;
 
-        > li {
+        > .tag {
             min-height: 44px;
             display: flex;
             border-bottom: 1px solid #f5f5f5;
