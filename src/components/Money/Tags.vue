8f47e6f8943f0f5e77072f8a5ce1d45ca98ef883
huomeng <455801665@qq.com>
2020-08-19 20:49:55 +0800

    所有computed改成get函数

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,17 +19,15 @@
   import TagHelper from '@/mixins/TagHelper';
 
 
-  @Component({
-    computed: {
-      tagList() {
-        return this.$store.state.tagList;
-      }
-    }
-  })
+  @Component
 
   export default class Tags extends mixins(TagHelper) {
 
     selectedTags: string[] = [];
+
+    get tagList() {
+      return this.$store.state.tagList;
+    }
 
     created() {
       return this.$store.commit('fetchTags');
