84ac1a871445d5e848887ca5407b81ffa9b4777c
mr.huo <455801665@qq.com>
2020-08-27 21:41:52 +0800

    修复一些bug

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -4,10 +4,10 @@
         <div class="notes">
             <FormItem field-name="备注"
                       placeholder="在这里输入备注"
-                      @update:value="onUpdateNotes"
+                      :value.sync="record.notes"
             />
         </div>
-        <Tags/>
+        <Tags @update:value="record.tags=$event" />
         <Tabs :data-source="recordTypeList"
               :value.sync="record.type"/>
     </Layout>
@@ -49,7 +49,15 @@
     }
 
     saveRecord() {
+      if (!this.record.tags || this.record.tags.length === 0) {
+        return window.alert('请至少选择一个标签');
+      }
       this.$store.commit('createRecord', this.record);
+      if (this.$store.state.createRecordError === null) {
+        window.alert('记账成功');
+        this.record.notes = '';
+      }
+
     }
 
   }
